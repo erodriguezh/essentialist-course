2d169c108faaca4bf2ce655412d78556f19c4a8c
Eduardo Rodriguez <erodriguez@greentube.com>
2023-03-24 11:25:48 +0100

    green - Add a test for 1st requirement: A function for fizzbuzz challenge.  Add  function fizzbuzz.

---
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -0,0 +1,2 @@
+export function fizzbuzz(iterationNumber: number) {
+}
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,7 @@
+import { fizzbuzz } from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
-
-
+  it("should be a function", () => {
+    expect(typeof fizzbuzz).toBe("function");
+  });
 });
