6901d75dc475b64abf09f8a93807f09cf29c8ed6
Eduardo Rodriguez <erodriguez@greentube.com>
2023-03-24 11:29:06 +0100

    green - throw an error if argument is not a number between 1 and 100

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,2 +1,5 @@
 export function fizzbuzz(iterationNumber: number) {
+  if (typeof iterationNumber !== 'number' || iterationNumber < 1 || iterationNumber > 100) {
+    throw new Error('You must provide a number between 1 and 100');
+  }
 }
