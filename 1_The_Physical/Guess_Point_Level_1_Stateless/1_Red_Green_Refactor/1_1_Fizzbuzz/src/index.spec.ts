c1b6ef4eb03c7aef2bb18fbb34e6e1b7cf2da104
Eduardo Rodriguez <erodriguez@greentube.com>
2023-03-24 11:28:05 +0100

    Red -  Add test for 2nd requirement: function takes a number between 1 and 100 as argument

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -4,4 +4,10 @@ describe("fizzbuzz", () => {
   it("should be a function", () => {
     expect(typeof fizzbuzz).toBe("function");
   });
+  it("should not take numbers under 1", () => {
+    expect(() => fizzbuzz(0)).toThrow("You must provide a number between 1 and 100");
+  });
+  it("should not take numbers over 100", () => {
+    expect(() => fizzbuzz(101)).toThrow("You must provide a number between 1 and 100");
+  });
 });
