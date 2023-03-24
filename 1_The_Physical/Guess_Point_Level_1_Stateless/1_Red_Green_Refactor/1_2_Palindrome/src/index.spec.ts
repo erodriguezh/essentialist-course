566c7a02d9d2f8f260af6fb4a7d999aa2faf8f87
Eduardo Rodriguez <erodriguez@greentube.com>
2023-03-24 13:19:46 +0100

    red - add test for requirement: Detects that a word is a palindrome or not

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -5,4 +5,10 @@ describe('palindrome checker', () => {
   it("should be a function", () => {
     expect(typeof palindromeChecker).toBe("function");
   });
+  it("should detect that a word is a palindrome", () => {
+    expect(palindromeChecker('wow')).toBeTruthy();
+  });
+  it("should detect that a word is not a palindrome", () => {
+    expect(palindromeChecker('bill')).toBeTruthy();
+  });
 })
